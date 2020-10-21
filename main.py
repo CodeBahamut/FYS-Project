0ff27c5777a83e964e2f32db79c1878f9fb4f6d4
R.A. de Jong <roan.de.jong@hva.nl>
2020-10-21 13:58:33 +0200

    Als je op de knop vooruit drukt, dan wordt je geredirect naar een andere html template waarop de status staat van de robot. Onder de comments van de NIET status routes kan je dus de GPIO pinnen op HIGH en LOW zetten. Binnen de status routes hoef je niks te veranderen.

--- main.py
+++ main.py
@@ -6,27 +6,43 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    #Hierin kan je de setups van de GPIO pinnen zetten
     return render_template('index.html')
 
 @app.route("/Vooruit")
 def vooruit():
     #Hierin komt dus de GPIO, HIGH voor wanneer de knop 'vooruit' wordt ingedrukt
-    return redirect("/")
+    return redirect("/StatusVooruit")
 
 @app.route("/Achteruit")
 def achteruit():
     #Hierin komt de GPIO, HIGH voor wanneer de knop 'achteruit' wordt ingedrukt
-    return redirect("/")
+    return redirect("/StatusAchteruit")
 
 @app.route("/Links")
 def links():
     #Hierin komt de GPIO, HIGH voor wanneer de knop 'links' wordt ingedrukt
-    return redirect("/")
+    return redirect("/StatusLinks")
 
 @app.route("/Rechts")
 def rechts():
     #Hierin komt de GPIO, HIGH voor wanneer de knop 'rechts' wordt ingedrukt
     return redirect("/")
+
+@app.route("/StatusVooruit")
+def statusvrt():
+    #De html pagina waarin de geupdate status staat
+    return render_template("statusvrt.html")
+
+@app.route("/StatusAchteruit")
+def statusact():
+    #De html pagina waarin de geupdate status staat
+    return render_template("statusact.html")
+
+@app.route("/StatusLinks")
+def statuslnk():
+    #De html pagina waarin de geupdate status staat
+    return render_template("statuslnk.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
