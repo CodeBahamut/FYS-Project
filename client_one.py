d1ae24770fe0d588cdfefabc3da43a60f367ede3
M.B. Koopdonk <maurits.koopdonk@hva.nl>
2020-12-08 13:36:58 +0100

    Update client_one.py

--- client_one.py
+++ client_one.py
@@ -14,3 +14,68 @@ while 1:
         break
     s.send(bytes(text, 'UTF-8'))
 s.close()
+
+while id != 914171570937 and id != 848246612969:
+
+        import time
+        import RPi.GPIO as GPIO
+        from mfrc522 import SimpleMFRC522
+
+        reader = SimpleMFRC522()
+
+        GPIO.setwarnings(False)
+        GPIO.setup(12, GPIO.OUT)
+        GPIO.setup(37, GPIO.OUT)
+        GPIO.output(37, GPIO.LOW)
+        GPIO.output(12, GPIO.HIGH)
+
+        id, text = reader.read()
+        print(id)
+
+        if id == 914171570937:
+                GPIO.output(12, GPIO.LOW)
+                score = score +1
+                print(score)
+
+
+        elif id == 848246612969:
+
+                GPIO.output(12, GPIO.LOW)
+                start_time = time.time()
+                seconds = 4
+
+                while True:
+                        current_time = time.time()
+                        elapsed_time = current_time - start_time
+                        GPIO.output(37, GPIO.HIGH)
+                        time.sleep(0.1)
+                        GPIO.output(37, GPIO.LOW)
+                        time.sleep(0.1)
+
+                        if elapsed_time > seconds:
+                                break
+
+        else:
+
+                start_time = time.time()
+                seconds = 4
+
+                while True:
+                        current_time = time.time()
+                        elapsed_time = current_time - start_time
+
+                        GPIO.output(37, GPIO.HIGH)
+                        GPIO.output(12, GPIO.HIGH)
+                        time.sleep(0.1)
+                        GPIO.output(37, GPIO.LOW)
+                        GPIO.output(12, GPIO.LOW)
+                        time.sleep(0.1)
+
+                        if elapsed_time > seconds:
+                                break
+
+GPIO.cleanup()
+send1 = str(score)
+print(send1)
+s.send(bytes(send1, 'UTF-8'))
+s.close()
