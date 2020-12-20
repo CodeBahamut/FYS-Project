5ae03658c5bae020dcdf5572af03a125bbe67d6b
DESKTOP-BIRKB4B\Kelada <benjamin.kelada@hva.nl>
2020-12-20 16:01:57 +0100

    Uitleg erbij

--- database.py
+++ database.py
@@ -8,15 +8,17 @@ database = mysql.connector.connect(
 )
 def main():
 
-
+# hier laat je met cursor.execute zien wat je in je database wilt zetten en welke values het heeft
     cursor = database.cursor()
     cursor.execute("INSERT INTO`Fys` (`name`, `score`) "
                "VALUES('bart', '5' );")
 
     database.commit()
 
+# hiermee laat je zien wat je wilt hebben uit je database
     cursor.execute("SELECT`name`, `score` FROM`Fys`")
 
+# hiermee pak je alles uit naam en score 
     result = cursor.fetchall()
 
     for row in result:
